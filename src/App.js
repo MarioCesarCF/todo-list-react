29355c17ddb8fa71c7f0f72ce61a1d13fbbb97c8
Mário Cesar <mc.fialho@hotmail.com>
2022-09-28 00:29:29 -0300

    finalizando

--- src/App.js
+++ src/App.js
@@ -5,25 +5,81 @@ import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 const API = "http://localhost:5000";
 
 function App() {
-const [title, setTitle] = useState("");
-const [time, setTime] = useState("");
-const [todos, setTodos] = useState([]);
-const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  
-  const todo = {
-    id: Math.random(),
-    title,
-    time,
-    done: false,
-  };
-  console.log(todo);
-  
-  setTitle("");
-  setTime("");
-}
+  //load todos on page land
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true)
+      //setloading altera o estado
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err))
+
+      setLoading(false);
+
+      setTodos(res);
+    };
+
+    loadData();
+    //Não entendi pq já colocar o setloading(false) e nem pq a função tem com segundo parametro um array vazio
+  }, [])
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    //esse prevState é usado para carregar o que tinha anteriormente, sem precisar fazer novas requisições na API
+    setTodos((prevState) => [...prevState, todo]);
+
+    setTitle("");
+    setTime("");
+  }
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  }
+
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+  }
+
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className="App">
@@ -46,6 +102,18 @@ const handleSubmit = (e) => {
       <div className="list-todo">
         <h2>Lista de tarefas:</h2>
         {todos.length === 0 && <p>Não há tarefas.</p>}
+        {todos.map((todo) => (
+          <div className='todo' key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className='actions'>
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
